d5136736430ab08e4a24549ac994452c376e876c
shubhamagrawal902785 <shubhamag937@gmail.com>
2022-11-07 00:00:11 +0530

    Added: Write a program in oops concepts

--- src/com/lincomparison/Length.java
+++ src/com/lincomparison/Length.java
@@ -6,43 +6,50 @@ public class Length {
 
 	
 
+	 static double length(int x1,int y1, int x2, int y2){
+	        double length = Math.sqrt((double)((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+	        System.out.println("The line length 1 is :" + length);
+	        return length;
+	    }
+	
+	
 
-    public static void main(String[] args) {
+	    public static void main(String[] args) {
 
-        Scanner Sc = new Scanner(System.in);
+	        Scanner Sc = new Scanner(System.in);
 
-        // input co=ordinate of line 1
-        System.out.println("Enter the value of X1 in line 1");
-        int x1 = Sc.nextInt();
-        System.out.println("Enter the value of Y1 in line 1");
-        int y1 = Sc.nextInt();
-        System.out.println("Enter the value of X2 in line 1");
-        int x2 = Sc.nextInt();
-        System.out.println("Enter the value of Y2 in line 1");
-        int y2 = Sc.nextInt();
+	        // input co=ordinate of line 1
+	        System.out.println("Enter the value of X1 in line 1");
+	        int x1 = Sc.nextInt();
+	        System.out.println("Enter the value of Y1 in line 1");
+	        int y1 = Sc.nextInt();
+	        System.out.println("Enter the value of X2 in line 1");
+	        int x2 = Sc.nextInt();
+	        System.out.println("Enter the value of Y2 in line 1");
+	        int y2 = Sc.nextInt();
 
-        // input co=ordinate of line 1
-        System.out.println("Enter the value of X1 in line 2");
-        int a1 = Sc.nextInt();
-        System.out.println("Enter the value of Y1 in line 2");
-        int b1 = Sc.nextInt();
-        System.out.println("Enter the value of X2 in line 2");
-        int a2 = Sc.nextInt();
-        System.out.println("Enter the value of Y2 in line 2");
-        int b2 = Sc.nextInt();
+	        // input co=ordinate of line 2
+	        System.out.println("Enter the value of X1 in line 2");
+	        int a1 = Sc.nextInt();
+	        System.out.println("Enter the value of Y1 in line 2");
+	        int b1 = Sc.nextInt();
+	        System.out.println("Enter the value of X2 in line 2");
+	        int a2 = Sc.nextInt();
+	        System.out.println("Enter the value of Y2 in line 2");
+	        int b2 = Sc.nextInt();
 
-        double length1 = Math.sqrt((double)((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
-        System.out.println("The line length 1 is :" + length1);
-        double length2 = Math.sqrt((double)((a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1)));
-        System.out.println("The line length 2 is :" + length2);
+	        Length obj = new Length();
+	        double l1 = obj.length(x1,y1,x2,y2);
+	        double l2 = obj.length(a1, b1,a2,b2);
 
-        if (length1 > length2) {
-            System.out.println(" Line 1 is grater than line 2");
-        } else if (length1 < length2) {
-            System.out.println("Line 1 is shorter than line 2");
-        } else {
-            System.out.println("Both lines are equal ");
-        }
+	        String ln1 = Double.toString(l1);
+	        String ln2 = Double.toString(l2);
 
-    }
-}
+	        if (ln1.compareTo(ln2) < 0) {
+	            System.out.println(" Line 1 is grater than line 2");
+	        } else if (ln1.compareTo(ln2) > 0) {
+	        } else if (ln1.compareTo(ln2) == 0){
+	            System.out.println("Both lines are equal ");
+	        }
+	    }
+	}
